be5e5d300044e0e018fc176dc3f4fb5529897353
Mary <mitroimarianavalentina1986@gmail.com>
2020-11-17 23:02:07 +0200

    corectat Api-ul

--- fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/controller/UserController.java
+++ fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/controller/UserController.java
@@ -6,17 +6,40 @@ import org.springframework.web.bind.annotation.RestController;
 import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 import ro.agilehub.javacourse.car.hire.api.specification.UserApi;
 
+import javax.validation.Valid;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-public class SampleController implements UserApi {
+public class UserController implements UserApi {
 
     @Override
     public ResponseEntity<List<UserDTO>> getUsers() {
         UserDTO userDTO = new UserDTO();
         userDTO.setId(1);
-        userDTO.setUserName("My first sample");
+        userDTO.setUserName("Second User");
         return ResponseEntity.ok(Collections.singletonList(userDTO));
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> addUser(@Valid UserDTO userDTO) {
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> getUser(Integer id) {
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> removeUser(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<UserDTO> updateUser(Integer id, @Valid UserDTO userDTO) {
+        return null;
     }
 }

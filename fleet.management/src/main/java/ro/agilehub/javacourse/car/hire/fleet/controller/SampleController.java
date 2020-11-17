d7e774ec4b7a559884c7389638146f9783e0ef66
Mary <mitroimarianavalentina1986@gmail.com>
2020-11-17 14:54:11 +0200

    tema 3 ready

--- fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/controller/SampleController.java
+++ fleet.management/src/main/java/ro/agilehub/javacourse/car/hire/fleet/controller/SampleController.java
@@ -3,20 +3,20 @@ package ro.agilehub.javacourse.car.hire.fleet.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import ro.agilehub.javacourse.car.hire.api.model.SampleDTO;
-import ro.agilehub.javacourse.car.hire.api.specification.SampleApi;
+import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
+import ro.agilehub.javacourse.car.hire.api.specification.UserApi;
 
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-public class SampleController implements SampleApi {
+public class SampleController implements UserApi {
 
     @Override
-    public ResponseEntity<List<SampleDTO>> getSamples() {
-        SampleDTO sampleDTO = new SampleDTO();
-        sampleDTO.setId(1);
-        sampleDTO.setName("My first sample");
-        return ResponseEntity.ok(Collections.singletonList(sampleDTO));
+    public ResponseEntity<List<UserDTO>> getUsers() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setUserName("My first sample");
+        return ResponseEntity.ok(Collections.singletonList(userDTO));
     }
 }
